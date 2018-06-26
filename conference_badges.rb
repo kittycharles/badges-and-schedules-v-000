f1558c8ccbb5c40299d762f070bb04aef3425830
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-26 17:25:57 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ end
 
 def assign_rooms(attendees)
   room=0
-  attendees.each do |name|
+  attendees.collect do |name|
     room+=1
     "Hello, #{name}! You'll be assigned to room #{room}!"
   end
