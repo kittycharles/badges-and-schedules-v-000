144e5b744d10941a0deef4cb57da1d817ec3ef28
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-26 17:32:17 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,10 +17,10 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each do |value|
-    puts value
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
   end
-  assign_rooms(attendees).each do |value|
-    puts value
+  assign_rooms(attendees).each do |room|
+    puts room
   end
 end
