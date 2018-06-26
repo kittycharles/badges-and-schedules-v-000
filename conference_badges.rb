9e101ec4fdf691480d3fd9f844092046bddf7cf7
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-26 17:09:01 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.collect do |name|
+    badge_maker(name)
+  end
+end
+
+def assign_rooms(attendees)
+  counter = 1
+  attendees.collect do |name|
+    puts "Hello, #{name}! You'll be assigned to room #{counter}!"
+    counter += 1
+end
